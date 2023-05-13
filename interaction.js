ca2567abab87d83b8d1fbfd45442aa8da0f1b70d
Valentýna Kociánová <123517319+entyna@users.noreply.github.com>
2023-05-13 02:07:14 +0200

    css graph toggles

--- interaction.js
+++ interaction.js
@@ -10,14 +10,29 @@ document.body.appendChild(buttonContainer);
 let buttons = [];
 for (let i = 0; i < 6; i++) {
   let button = document.createElement("button");
-  //button.innerHTML = "1";
+  button.classList.add("button-" + yaoValues[i]); // Add initial class based on yaoValue
   button.addEventListener("click", function() {
     // Toggle the yaoValues value for this button
     yaoValues[i] = 1 - yaoValues[i];
-    //button.innerHTML = yaoValues[i].toString();
+    button.classList.toggle("button-0"); // Toggle class for yaoValue 0
+    button.classList.toggle("button-1"); // Toggle class for yaoValue 1
     // Call the updatePoints function in sketch.js, passing in the updated yaoValues array
     updatePoints(yaoValues);
   });
   buttons.push(button);
   buttonContainer.appendChild(button);
 }
+
+// // Create the buttons
+// let buttons = [];
+// for (let i = 0; i < 6; i++) {
+//   let button = document.createElement("button");
+//   button.addEventListener("click", function() {
+//     // Toggle the yaoValues value for this button
+//     yaoValues[i] = 1 - yaoValues[i];
+//     // Call the updatePoints function in sketch.js, passing in the updated yaoValues array
+//     updatePoints(yaoValues);
+//   });
+//   buttons.push(button);
+//   buttonContainer.appendChild(button);
+// }
